eff556943c51e41b82909bf52412446f7a2416ac
Davide Cirillo <cirillod@gmail.com>
2020-01-21 15:40:12 +0100

    Updated xml_processing.py

--- lib/xml_processing.py
+++ lib/xml_processing.py
@@ -1,15 +1,36 @@
 from bs4 import BeautifulSoup
-import pickle
+import pickle, os
 from tqdm import tqdm
  
+def clean_xml(in_xml,out_xml):
+
+    input = open(in_xml,"r",encoding='utf-8')
+    output = open(out_xml,"w",encoding='utf-8')
+
+    output.write(input.readline())
+    output.write("<PubmedArticleSet>")
+
+    tpl = ("<PubmedArticleSet>","</PubmedArticleSet>","<!DOCTYPE","<?xml")
+    for line in input:
+        if not line.lstrip().startswith(tpl):
+            output.write(line)
+
+    output.write("</PubmedArticleSet>")
+
+    input.close()
+    output.close()
+
 def parse_xml(xml_file,out_pkl):
 	
 	dictPMID = {}
-	infile = open(xml_file,"r",encoding='utf-8')
+	xml_clean_file = "{0}_{2}{1}".format((*os.path.splitext(xml_file) + ('clean',)))
+	clean_xml(xml_file,xml_clean_file)
+	infile = open(xml_clean_file,"r",encoding='utf-8')
 	contents = infile.read()
-	soup = BeautifulSoup(contents, 'lxml')
+	soup = BeautifulSoup(contents,'lxml-xml')
 
 	articles = soup.select('PubmedArticle')
+	print(len(articles))
 	for i in tqdm(range(len(articles))):
 		article = articles[i]
 		pmid = article.select_one('PMID').text
